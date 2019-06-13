d3e7949f5fae38f8b30d5207d31efd2c507ab0e2
Corneliu <corneliu.gaina@gmail.com>
2019-06-13 16:55:13 +0200

    Updates

---
+++ precache-manifest.771fd23ebf7147daa196344ae76dccad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f410d10fe3b08cdfa4d9901d2f470d",
+    "url": "/pilotemedia_collaborative_journalism/index.html"
+  },
+  {
+    "revision": "52f78c3fe39c1e464b52",
+    "url": "/pilotemedia_collaborative_journalism/static/css/main.d7ee812e.chunk.css"
+  },
+  {
+    "revision": "b1017e0f1b0a2f02a7d0",
+    "url": "/pilotemedia_collaborative_journalism/static/js/2.af490cf8.chunk.js"
+  },
+  {
+    "revision": "52f78c3fe39c1e464b52",
+    "url": "/pilotemedia_collaborative_journalism/static/js/main.28e03a81.chunk.js"
+  },
+  {
+    "revision": "49fff9e34d2c69d1f426",
+    "url": "/pilotemedia_collaborative_journalism/static/js/runtime~main.c03101e7.js"
+  }
+]);
